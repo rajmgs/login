111b4314832660d18d556087c2c3116198b66a06
Raj <rajc16639@gmail.com>
2021-10-16 14:42:03 +0530

    initial commit

--- src/main/java/com/login/demo/DemoApplication.java
+++ src/main/java/com/login/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.login.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/test/java/com/login/demo/DemoApplicationTests.java
+++ src/test/java/com/login/demo/DemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.login.demo;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
