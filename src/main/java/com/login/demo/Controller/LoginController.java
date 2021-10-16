9b2b8e584efc86d8594844b0d5a6084e62c522a8
Raj <kumarchaurasiaraj@gmail.com>
2021-10-16 13:00:36 +0530

    initial commit

---
+++ src/main/java/com/login/demo/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.login.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping({"/"})
+	public String first() {
+		return "index";
+	}
+
+}
+
